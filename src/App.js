f6c7c5bc68a7b95470c60e4c992534dd55469924
xarlys <xarlysouza@hotmail.com>
2020-04-09 13:29:34 -0300

    Finalizado

--- src/App.js
+++ src/App.js
@@ -1,29 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
+
+
+
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
     // TODO
+    const response = await api.post('/repositories', {
+      title: `Novo Respositorio ${Date.now()}`,
+      url: "https://github.com/xarlys",
+      techs: ["ReactJS, NodeJS"]
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);   
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    // TODO 
+    const repositoriesIndex = repositories.findIndex(repository=> repository.id === id)
+
+    await api.delete(`/repositories/${id}`);
+
+    repositories.splice(repositoriesIndex, 1)
+
+    setRepositories([...repositories]); 
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+      <h1>Desafio 03: Conceitos do ReactJS</h1>
+      <br/>
+      <div id="ul">
+        
+        <ul data-testid="repository-list">
+          {repositories.map(repository => 
+            <li key={repository.id}>
+              {repository.title} 
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          )}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+        </ul>
+        <br/>      
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </div>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      
+    
     </div>
   );
 }
